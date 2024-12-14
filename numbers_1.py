5128b06f9a1fdfce141fb218009a240463d9727b
Greefe <Greef@1.com>
2024-12-14 22:55:18 +0300

    add operation with number

---
+++ numbers_1.py
@@ -0,0 +1,14 @@
+a = 1
+b = 5
+c = 100
+e = 2.411
+total = a + b + c
+print(total)
+total = a + b + c + e
+print(total)
+total = c // 5
+print(total)
+total = c % 5
+print(total)
+from math import ceil, floor
+print('обычное округление', round(e),'\n', 'округление вверх', ceil(e), '\n', 'округление вниз', floor(e))

928fec16ed1e2fac861b19f37b39885686922b64
GrinEvg <Greef@bk.ru>
2024-12-16 19:30:44 +0300

    add home work

---
+++ string_test.py
@@ -0,0 +1,49 @@
+# задание 1
+data = '42'
+integer_data = int(data)
+float_data = float(data)
+string_data = str(data)
+list_data = list(data)
+print(data)
+print(float_data)
+print(string_data)
+print(list_data)
+
+# задание 2
+name = 'Иван'
+age = '25'
+
+# задание 3
+word = 'Привет!'
+repeated_word = word * 3
+print(repeated_word)
+
+# задание 4
+first_name = 'Иван'
+last_name = 'Иванов'
+full_name = first_name + ' ' + last_name
+
+# задание 5
+num = 1.5e-57
+s = str(num)
+
+# задание 6
+hello_word = 'Привет, мир!'
+length = len(hello_word)
+print(length)
+
+# задание 7
+my_string = 'Это строка с "внутренними" кавычками.'
+print(my_string)
+
+# задание 8
+special_string = "Привет, мир!\nPython\tЯ изучаю \"язык программирования\"!"
+print(special_string)
+
+# задание 9
+multi_line_string = '''
+Это многострочная строка.
+Она состоит из нескольких строк текста.
+Вот здесь новая строка.
+'''
+print(multi_line_string)
